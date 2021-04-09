ab19d5c6a48d8213c49d3eabb93dbb374a8b45fd
MarianaRosaluz <marianarosaluz@yahoo.com>
2021-04-08 22:50:43 -0300

    refactoring product class and implementing an interface that can be closed in the service

--- ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -9,33 +9,17 @@ import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
-    public  static void main(String [] ars) throws ExecutionException, InterruptedException {
-        var producer = new KafkaProducer<String, String>(properties());
-
-        for (var i = 0; i<100;i++) {
-            var key = UUID.randomUUID().toString();
-            var value = key + "123123,131";
-            var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
-            Callback callback = (data, ex) -> {
-                if (ex != null) {
-                    ex.printStackTrace();
-                    return;
-                }
-                System.out.println("Sucesso enviando " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timeStamp " + data.timestamp());
-            };
-            var email = "Welcome! we are processing your order";
-            var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", "email", "email");
-            producer.send(record, callback).get();
-            producer.send(emailRecord, callback).get();
+    public static void main(String[] ars) throws ExecutionException, InterruptedException {
+        try (var dispatcher = new KafkaDispatcher()) {
+            for (var i = 0; i < 100; i++) {
+                var key = UUID.randomUUID().toString();
+                var value = key + "123123,131";
+                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                var email = "Thank you for your order! we are processing your order";
+                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+            }
         }
     }
 
-    private static Properties properties(){
-        var properties = new Properties();
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
-        return properties;
-    }
 
 }
