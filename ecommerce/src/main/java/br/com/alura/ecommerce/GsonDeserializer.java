de2817909d03a698e77c7e18c81ae2d5ab11a840
MarianaRosaluz <marianarosaluz@yahoo.com>
2021-04-12 23:59:34 -0300

    implementing

---
+++ ecommerce/src/main/java/br/com/alura/ecommerce/GsonDeserializer.java
@@ -0,0 +1,22 @@
+package br.com.alura.ecommerce;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.kafka.common.serialization.Deserializer;
+
+import java.util.Map;
+
+public class GsonDeserializer<T> implements Deserializer<T> {
+
+    private final Gson gson = new GsonBuilder().create();
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        Deserializer.super.configure(configs, isKey);
+    }
+
+    @Override
+    public T deserialize(String s, byte[] bytes) {
+        return gson.fromJson(bytes,type);
+    }
+}
