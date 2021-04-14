c3c1efdcb36e9554ce89aa189355f47fbea673b4
MarianaRosaluz <marianarosaluz@yahoo.com>
2021-04-13 22:59:12 -0300

    customized deserialization according to the layer

--- ecommerce/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
+++ ecommerce/src/main/java/br/com/alura/ecommerce/FraudDetectorService.java
@@ -1,6 +1,7 @@
 package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import java.util.Map;
 
 public class FraudDetectorService {
 
@@ -8,7 +9,8 @@ public class FraudDetectorService {
         var fraudService = new FraudDetectorService();
         try (var service = new KafkaService<>(FraudDetectorService.class.getSimpleName(), "ECOMMERCE_NEW_ORDER",
                 fraudService::parse,
-                Order.class)) {
+                Order.class,
+                Map.of())) {
             service.run();
         }
     }
